f845f8d344d83c112b82d776fd2251244d49ba60
Suz4nGG <scxg240@gmail.com>
2022-12-06 17:46:26 -0600

    Form

---
+++ components/Forms/Select.jsx
@@ -0,0 +1,14 @@
+const Select = () => {
+  return (
+    <div>
+      <label>
+        Tipo de discapacidad
+      </label>
+      <select>
+        <option>{optText}</option>
+      </select>
+    </div>
+  );
+}
+
+export default Select;
