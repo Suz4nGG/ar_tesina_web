146a7186a1cb4bf1c5c23019e4b9c939c35ad01d
Suz4nGG <scxg240@gmail.com>
2023-01-02 19:56:09 -0600

    Se agregaron nuevos breakpoints

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,22 @@
 /** @type {import('tailwindcss').Config} */
+
 module.exports = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}"
   ],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        'mo': '300px',
+        'mob': '400px',
+        'sm': '640px',
+        'md': '768px',
+        'lg': '1024px',
+        'xl': '1280px',
+        '2xl': '1536px',
+      },
+    },
   },
   plugins: [],
 }
