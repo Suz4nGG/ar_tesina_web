15c206ddf6428abfdeab8505a93a19497916b023
Suz4nGG <scxg240@gmail.com>
2023-01-09 00:15:10 -0600

    Refactorizando página

--- pages/index.js
+++ pages/index.js
@@ -1,28 +1,20 @@
-import { Container } from "../components/Global/Container"
-import Footer from "../components/Global/Footer"
-import PageTitle from "../components/Global/PageTitle"
-import Hero from "../components/Home/Hero"
-import Info from "../components/Home/Info"
-import Logos from "../components/Home/Logos"
-import { PageProvider } from "./context/pagesContext"
-import Head from "next/head"
+import { Container } from "../components/Global/Container";
+import Footer from "../components/Global/Footer";
+import PageTitle from "../components/Global/PageTitle";
+import Hero from "../components/Home/Hero";
+import Info from "../components/Home/Info";
+import Logos from "../components/Home/Logos";
 
 const Index = () => {
   return (
-    <PageProvider>
-      <Container>
-        <PageTitle
-          title="Inicio"
-          name="Inicio"
-          content="Inicio aplicación"
-        />
-        <Hero />
-        <Info />
-        <Logos />
-        <Footer/>
-      </Container>
-    </PageProvider>
-  )
-}
+    <Container>
+      <PageTitle title="Inicio" name="Inicio" content="Inicio aplicación" />
+      <Hero />
+      <Info />
+      <Logos />
+      <Footer />
+    </Container>
+  );
+};
 
-export default Index
+export default Index;
