dc46559e094f360fa74b0b42b5b31bb5b646f197
Suz4nGG <scxg240@gmail.com>
2023-01-09 00:12:45 -0600

    Nuevo modelo de solicitud de adaptación del estudiante

--- database/db.sql
+++ database/db.sql
@@ -5,7 +5,7 @@ USE ar_data;
 CREATE TABLE estudiantes (
   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   username VARCHAR(5) NOT NULL,
-  passwordU VARCHAR(30) NOT NULL,
+  passwordU VARCHAR(400) NOT NULL,
   nombreCompleto VARCHAR(200) NOT NULL,
   nombreResponsable VARCHAR(200) NOT NULL,
   fecNacimiento DATE NOT NULL,
@@ -22,4 +22,18 @@ CREATE TABLE estudiantes (
   createdAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
-DESCRIBE estudiantes;
+CREATE TABLE solicitudAdaptacion (
+  idSolicitud INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  username VARCHAR(5) NOT NULL,
+  informacion VARCHAR(2000),
+  respuesta VARCHAR(2000),
+  tiempoHorario VARCHAR(2000),
+  adapAnteriores VARCHAR(2000),
+  motSolicitud VARCHAR(2000) NOT NULL,
+  experienciaR VARCHAR(200) NOT NULL,
+  estadoSolicitud INT NOT NULL DEFAULT 1,
+  createdAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+DROP TABLE solicitudAdaptacion;
+DESCRIBE estudiantes solicitudAdaptacion;
