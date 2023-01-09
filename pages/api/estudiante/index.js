d2914e941ace03277862d89b570702cfd058ab1f
Suz4nGG <scxg240@gmail.com>
2023-01-09 00:16:43 -0600

    ENDPOINT datos del estudiante

---
+++ pages/api/estudiante/index.js
@@ -0,0 +1,22 @@
+import { validateToken } from "../../../hooks/validateToken";
+import { pool } from "../../../config/db";
+
+export default async function getDataStudent(req, res) {
+  switch (req.method) {
+    case "POST":
+      await validateTK(req, res)
+    break
+  }
+}
+
+const validateTK = async (req, res) => {
+  const { authTokenUser } = req.body
+  const username = validateToken(authTokenUser)
+  const [result] = await pool.query(
+      "SELECT * FROM estudiantes WHERE username = ?",
+      [username]
+    );
+      return res.json(
+      result
+    );
+}
