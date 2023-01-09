995d5c71f1c3a24647a2aaf99f87f492c0cfd11e
Suz4nGG <scxg240@gmail.com>
2023-01-09 00:14:29 -0600

    Rutas de la aplicación

--- pages/constants.js
+++ pages/constants.js
@@ -1,1 +1,22 @@
 export const API_REG = "http://localhost:3000/api/registro"
+
+export const LOGIN = "/login"
+export const REGISTRO = "/registro/formulario"
+export const RECURSOS = "/recursos"
+export const CONTACTO = "/contacto"
+
+export const LOGINAUTH = "/api/auth/login-student"
+export const DASHSTUDENT = "/estudiante/dashboard"
+export const SOLSTUDENT = "/estudiante/solicitar-adaptacion"
+export const ADAPSTUDENT = "/estudiante/adaptaciones-curriculares"
+export const HERRACCESS = "/estudiante/herramientas-accesibles"
+export const DOCREQUIRED = "/estudiante/documentos-obligatorios"
+export const SOLADAPT = "/api/estudiante/solicitar-adaptacion"
+export const SOLADAPT2 = "/solicitar-adaptacion"
+export const GETADAPT = "/estudiante/adaptacion/"
+export const EDITADAPT = "/estudiante/editar-solicitud/"
+
+export const LOGOUT = "/api/auth/logout"
+export const TOKENJWT = 'authTokenUser'
+
+export const APISTUDENT = "http://localhost:3000/api/estudiante"
