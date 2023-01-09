10ba2afdb4feb68853f3605cd6f4e1bbd6b0198e
Suz4nGG <scxg240@gmail.com>
2023-01-09 00:04:02 -0600

    Protección de rutas con Middlewares

---
+++ middleware.js
@@ -0,0 +1,19 @@
+import { NextResponse } from "next/server";
+import { jwtVerify } from "jose";
+import { TOKENJWT } from "./pages/constants";
+const SECRET = process.env.SECRET
+
+export async function middleware(request) {
+  const jwt = request.cookies.get(TOKENJWT)
+  if (jwt === undefined) return NextResponse.redirect(new URL("/", request.url))
+  try {
+    await jwtVerify(jwt.value, new TextEncoder().encode(SECRET))
+    return NextResponse.next()
+  } catch (err) {
+    return NextResponse.redirect(new URL("/", request.url))
+  }
+}
+
+export const config = {
+  matcher: ["/estudiante/:path*",],
+};
