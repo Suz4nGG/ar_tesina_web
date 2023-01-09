5161931ed7b6b310942ae4e9319de690b870446c
Suz4nGG <scxg240@gmail.com>
2023-01-09 00:13:31 -0600

    Hook de anulación de JWT

---
+++ hooks/nullToken.js
@@ -0,0 +1,17 @@
+import { verify } from "jsonwebtoken"
+import { serialize } from "cookie"
+import { TOKENJWT } from "../pages/constants"
+const SECRET = process.env.SECRET
+
+export default function nullToken(token) {
+  const { authTokenUser } = token
+  verify(authTokenUser, SECRET)
+  const serialized = serialize(TOKENJWT, null, {
+    httpOnly: true,
+    secure: process.env.NODE_ENV === 'production',
+    sameSite: 'strict',
+    maxAge: 0,
+    path: '/'
+  })
+  return serialized
+}
