30b5508501cc4d43fb15cf78d529fdbb597d1315
Suz4nGG <scxg240@gmail.com>
2023-01-16 00:29:29 -0600

    Validación estudiante existente

--- pages/api/registro/index.js
+++ pages/api/registro/index.js
@@ -23,47 +23,63 @@ const registerStudent = async (req, res) => {
     tipoDiscapacidad,
     sobreDiscapacidad,
     carrera,
-    adaptaciones,
     tiempoDisc,
     temporal,
     permanente,
     usernameA,
-    password
+    password,
   } = req.body;
-  const salt = 10
-  const passHash = await bcrypt.hash(password, salt)
-  console.log("PWW",passHash)
-  const [result] = await pool.query("INSERT INTO estudiantes SET ?", {
-    nombreCompleto,
-    nombreResponsable,
-    fecNacimiento,
-    edad,
-    tel,
-    ciudad,
-    cp,
-    municipio,
-    tipoDiscapacidad,
-    sobreDiscapacidad,
-    carrera,
-    adaptaciones,
-    tiempoDisc: temporal || permanente,
-    usernameA,
-    passwordU: passHash
-  })
-  return res.status(200).json({
-    nombreCompleto,
-    nombreResponsable,
-    fecNacimiento,
-    edad,
-    tel,
-    ciudad,
-    cp,
-    municipio,
-    tipoDiscapacidad,
-    sobreDiscapacidad,
-    carrera,
-    adaptaciones,
-    tiempoDisc,
-    id: result.insertId
-  })
+  // ! Validar que el estudiante no tenga otra cuenta ya
+  try {
+    const [result2] = await pool.query(
+      "SELECT nombreCompleto, usernameA FROM estudiantes WHERE nombreCompleto = ? AND usernameA = ?",
+      [nombreCompleto, usernameA]
+    );
+    console.log(result2.length);
+    if (result2.length === 0) {
+      const salt = 10;
+      const passHash = await bcrypt.hash(password, salt);
+      const [result] = await pool.query("INSERT INTO estudiantes SET ?", {
+        nombreCompleto,
+        nombreResponsable,
+        fecNacimiento,
+        edad,
+        tel,
+        ciudad,
+        cp,
+        municipio,
+        tipoDiscapacidad,
+        sobreDiscapacidad,
+        carrera,
+        tiempoDisc: temporal || permanente,
+        usernameA,
+        passwordU: passHash,
+      });
+      return res.status(200).json({
+        nombreCompleto,
+        nombreResponsable,
+        fecNacimiento,
+        edad,
+        tel,
+        ciudad,
+        cp,
+        municipio,
+        tipoDiscapacidad,
+        sobreDiscapacidad,
+        carrera,
+        tiempoDisc,
+        id: result.insertId,
+      });
+    }
+    else {
+      return res
+        .status(401)
+        .json({
+          message:
+            "Revisa el nombre de usuario o nombre completo proporcionado, ya existe una cuenta con esos datos",
+        });
+    }
+  } catch (err) {
+    return res.status(500).json({message: err})
+  }
 };
