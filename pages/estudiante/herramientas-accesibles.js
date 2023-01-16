4170bfd6168b37029d30f07a3e5977bc8713e035
Suz4nGG <scxg240@gmail.com>
2023-01-16 00:30:40 -0600

    Ref

--- pages/estudiante/herramientas-accesibles.js
+++ pages/estudiante/herramientas-accesibles.js
@@ -2,6 +2,7 @@ import Navigation from "/components/Global/Navigation";
 import Layout from "/components/Global/Layout";
 import Herramientas from "./components/Herramientas";
 import { herramientasInfo,herramientasInfo2,herramientasInfo3 } from "./dataHerramientas";
+import Footer from "../../components/Global/Footer";
 
 const HerramientasAccesibles = () => {
   return (
@@ -10,7 +11,8 @@ const HerramientasAccesibles = () => {
       <Layout data={{ title: "Herramientas accesibles" }}>
         <Herramientas data={herramientasInfo} title="Lectores de pantalla" />
         <Herramientas data={herramientasInfo2} title="Traductores"/>
-        <Herramientas data={herramientasInfo3} title="Teclados virtuales"/>
+        <Herramientas data={herramientasInfo3} title="Teclados virtuales" />
+        <Footer/>
       </Layout>
     </div>
   );
