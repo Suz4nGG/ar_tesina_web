4a0a031c68d1a4d2ec8cd882122701f4d556b83d
Suz4nGG <scxg240@gmail.com>
2023-01-18 01:18:06 -0600

    ENDPOINT: COMENTARIOS DEL PA A LA SOLICITUD DEL ESTUDIANTE

--- pages/api/administrativo/comentar-adaptacion.js
+++ pages/api/administrativo/comentar-adaptacion.js
@@ -4,38 +4,73 @@ export default async function handler(req, res) {
   switch (req.method) {
     case "POST":
       return await comentarAdaptacion(req, res);
-    case "GET":
-      // await getStudent(req, res);
-      break;
   }
 }
 
 const comentarAdaptacion = async (req, res) => {
-  const { idSolicitud, comentariosMS } = req.body;
+  const {
+    idSolicitud,
+    comentarioInfo,
+    comentarioResp,
+    comentarioTH,
+    comentarioAA,
+    comentarioMS,
+  } = req.body;
   try {
     const [exist] = await pool.query(
-      "SELECT idSolicitud FROM comentarioSolicitud WHERE idSolicitud = ?" ,
+      "SELECT idSolicitud FROM comentarioSolicitud WHERE idSolicitud = ?",
       [idSolicitud]
     );
-    console.log(exist[0])
     if (exist[0]) {
-      console.log("EXISTE")
-      const [result] = await pool.query(
-        "INSERT INTO comentarioSolicitud SET ? WHERE idSolicitud = ?",
-        {...req.body}
-      );
-      console.log(result)
-      return res.status(200).json("mess");
+      switch (
+        comentarioInfo ||
+        comentarioResp ||
+        comentarioTH ||
+        comentarioAA ||
+        comentarioMS
+      ) {
+        case comentarioInfo:
+          const [result1] = await pool.query(
+            "UPDATE comentarioSolicitud SET comentarioInfo = ? WHERE idSolicitud = ?",
+            [comentarioInfo, idSolicitud]
+          );
+          return res.status(200).json(comentarioInfo);
+        case comentarioResp:
+          const [result2] = await pool.query(
+            "UPDATE comentarioSolicitud SET comentarioResp = ? WHERE idSolicitud = ?",
+            [comentarioResp, idSolicitud]
+          );
+          return res.status(200).json(comentarioResp);
+        case comentarioTH:
+          const [result3] = await pool.query(
+            "UPDATE comentarioSolicitud SET comentarioTH = ? WHERE idSolicitud = ?",
+            [comentarioTH, idSolicitud]
+          );
+          return res.status(200).json(comentarioTH);
+        case comentarioAA:
+          const [result4] = await pool.query(
+            "UPDATE comentarioSolicitud SET comentarioAA = ? WHERE idSolicitud = ?",
+            [comentarioAA, idSolicitud]
+          );
+          return res.status(200).json(comentarioAA);
+        case comentarioMS:
+          const [result5] = await pool.query(
+            "UPDATE comentarioSolicitud SET comentarioMS = ? WHERE idSolicitud = ?",
+            [comentarioMS, idSolicitud]
+          );
+          return res.status(200).json(comentarioMS);
+        default:
+          break;
+      }
     } else {
       const [result] = await pool.query(
         "INSERT INTO comentarioSolicitud SET ?",
-        {...req.body}
+        { ...req.body }
       );
-      console.log(result)
-      return res.status(200).json("mess");
+      return res.status(200).json(result[0]);
     }
   } catch (err) {
-    console.log(err)
+    console.log(err);
     return res.status(500).json({ message: "Error del servidor" });
   }
 };
