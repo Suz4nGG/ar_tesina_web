dd0b676f997159862b07359ab2ba3268254c0607
Suz4nGG <scxg240@gmail.com>
2023-01-23 23:40:16 -0600

    pool bd

--- config/db.js
+++ config/db.js
@@ -1,21 +1,21 @@
 const mysql = require("mysql2");
 
-//const pool = mysql
-//  .createPool({
-//    host: "localhost",
-//    user: "SCX24",
-//    password: "MYSQL24",
-//    port: 3306,
-//    database: "ar_data",
-//  })
-//  .promise();
+const pool = mysql
+  .createPool({
+    host: "localhost",
+    user: "SCX24",
+    password: "MYSQL24",
+    port: 3306,
+    database: "ar_data",
+  })
+  .promise();
 
-const pool = mysql.createPool({
-  host: 'localhost',
-  user: 'root',
-  password: 'mysqlpw',
-  port: 49153,
-  database: 'ar_data'
-}).promise()
+// const pool = mysql.createPool({
+//   host: 'localhost',
+//   user: 'root',
+//   password: 'mysqlpw',
+//   port: 49153,
+//   database: 'ar_data'
+// }).promise()
 
-export { pool }
+export { pool };
