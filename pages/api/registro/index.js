b0318431d293435f8349660ffd14846d35682ac4
Suz4nGG <scxg240@gmail.com>
2023-01-23 23:44:40 -0600

    Modifcando login, con el nuevo modelo de BD

--- pages/api/registro/index.js
+++ pages/api/registro/index.js
@@ -13,19 +13,11 @@ export default async function registro(req, res) {
 const registerStudent = async (req, res) => {
   const {
     nombreCompleto,
-    nombreResponsable,
-    fecNacimiento,
-    edad,
     tel,
-    ciudad,
-    cp,
-    municipio,
+    correo,
     tipoDiscapacidad,
     sobreDiscapacidad,
     carrera,
-    tiempoDisc,
-    temporal,
-    permanente,
     usernameA,
     password,
   } = req.body;
@@ -35,51 +27,39 @@ const registerStudent = async (req, res) => {
       "SELECT nombreCompleto, usernameA FROM estudiantes WHERE nombreCompleto = ? AND usernameA = ?",
       [nombreCompleto, usernameA]
     );
-    console.log(result2.length);
     if (result2.length === 0) {
       const salt = 10;
       const passHash = await bcrypt.hash(password, salt);
       const [result] = await pool.query("INSERT INTO estudiantes SET ?", {
         nombreCompleto,
-        nombreResponsable,
-        fecNacimiento,
-        edad,
         tel,
-        ciudad,
-        cp,
-        municipio,
+        correo,
         tipoDiscapacidad,
         sobreDiscapacidad,
         carrera,
-        tiempoDisc: temporal || permanente,
         usernameA,
         passwordU: passHash,
       });
       return res.status(200).json({
         nombreCompleto,
-        nombreResponsable,
-        fecNacimiento,
-        edad,
         tel,
-        ciudad,
-        cp,
-        municipio,
+        correo,
         tipoDiscapacidad,
         sobreDiscapacidad,
         carrera,
-        tiempoDisc,
+        usernameA,
         id: result.insertId,
       });
-    }
-    else {
-      return res
-        .status(401)
-        .json({
-          message:
-            "Revisa el nombre de usuario o nombre completo proporcionado, ya existe una cuenta con esos datos",
-        });
+    } else {
+      return res.status(401).json({
+        message:
+          "Revisa el nombre de usuario o nombre completo proporcionado, ya existe una cuenta con esos datos",
+      });
     }
   } catch (err) {
-    return res.status(500).json({message: err})
+    console.log(err)
+    return res
+      .status(500)
+      .json({ message: "Error del servidor intentelo más tarde" });
   }
 };
