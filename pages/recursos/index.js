59d7e68e92f6e8c9bdca7af594f01f86cf6cc09f
Suz4nGG <scxg240@gmail.com>
2023-01-30 20:05:59 -0600

    recursos

---
+++ pages/recursos/index.js
@@ -0,0 +1,21 @@
+import { Container } from "../../components/Global/Container"
+import PageTitle from "../../components/Global/PageTitle"
+import React from 'react';
+import Footer from "../../components/Global/Footer";
+import Herramientas from "../../components/Global/Herramientas";
+import { herramientasRecursos } from "./data";
+const Recursos = () => {
+  return (
+    <Container>
+      <PageTitle
+        title="Recursos"
+        name="Recursos"
+        content="Recursos sobre ajustes curriculares"
+      />
+      <Herramientas data={herramientasRecursos} showTitle={true} />
+      <Footer/>
+    </Container>
+  );
+}
+
+export default Recursos;
