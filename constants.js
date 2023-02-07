f556f56f713762ee8703790778d29e78a7de9f67
Suz4nGG <scxg240@gmail.com>
2023-02-06 20:04:24 -0600

    Fix: Order constants

--- constants.js
+++ constants.js
@@ -4,33 +4,36 @@ export const LOGIN = "/login";
 export const REGISTRO = "/registro/formulario";
 export const RECURSOS = "/recursos";
 export const CONTACTO = "/contacto";
-export const FORMSOL = "/estudiante/formulario-adaptacion";
+export const FORMULARIO_SOLICITUD = "/estudiante/formulario-adaptacion";
+export const URL_INICIAL = "http://localhost:3000";
 
-export const INITIAL = "http://localhost:3000/";
-export const REGISTROADMIN = "/api/registro/administrador";
-export const LOGINAUTHPERSONAL = "/api/auth/login-administrativo";
-export const DASHSPERSONAL = "/administrativo/dashboard";
-export const GETADAPTACIONES = "/api/administrativo/get-adaptaciones";
-export const GETADAPTPERSONAL = "/administrativo/adaptacion/";
-export const COMENTARADAP = "/api/administrativo/comentar-adaptacion/";
-export const CHANGESTATE = "/api/administrativo/cambiar-estado";
-export const GETCOMENTARIOS = "api/administrativo/comentarios/";
-export const LOGINAUTH = "/api/auth/login-student";
-export const DASHSTUDENT = "/estudiante/dashboard";
-export const SOLSTUDENT = "/estudiante/solicitar-adaptacion";
-export const ADAPSTUDENT = "/estudiante/adaptaciones-curriculares";
-export const HERRACCESS = "/estudiante/herramientas-accesibles";
-export const DOCREQUIRED = "/estudiante/documentos-necesarios";
-export const SOLADAPT = "/api/estudiante/solicitar-adaptacion";
-export const SOLADAPT2 = "/solicitar-adaptacion";
-export const GETADAPT = "/estudiante/adaptacion/";
-export const EDITADAPT = "/estudiante/editar-solicitud/";
+// & PERSONAL ADMINISTRATIVO
+export const REGISTRO_PERSONAL = "/api/registro/administrador";
+export const LOGIN_AUTH_PERSONAL = "/api/auth/login-administrativo";
+export const DASHBOARD_PERSONAL = "/administrativo/dashboard";
+export const GET_ADAPTACIONACIONES = "/api/administrativo/get-adaptaciones";
+export const GET_ADAPTACION_PERSONAL = "/administrativo/adaptacion/";
+export const COMENTAR_ADAPTACION = "/api/administrativo/comentar-adaptacion/";
+export const CAMBIAR_ESTADO_SOLICITUD = "/api/administrativo/cambiar-estado";
+export const GET_COMENTARIOS = "/api/administrativo/comentarios/";
+
+// & ESTUDIANTE
+export const LOGIN_AUTH_ESTUDIANTE = "/api/auth/login-student";
+export const DASHBOARD_ESTUDIANTE = "/estudiante/dashboard";
+export const SOLICITAR_ADAPTACION = "/estudiante/solicitar-adaptacion";
+export const ADAPTACIONES_ESTUDIANTE = "/estudiante/adaptaciones-curriculares";
+export const HERRAMIENTAS_ACCESIBLES = "/estudiante/herramientas-accesibles";
+export const DOCUMENTOS_REQUERIDOS = "/estudiante/documentos-necesarios";
+export const API_SOLICITAR_ADAPTACION = "/api/estudiante/solicitar-adaptacion";
+export const SOLICITAR_ADAPTACION2 = "/solicitar-adaptacion";
+export const GET_ADAPTACION = "/estudiante/adaptacion/";
+export const EDITAR_ADAPTACION = "/estudiante/editar-solicitud/";
 
 export const LOGOUT = "/api/auth/logout";
 export const TOKENJWT = "authTokenUser";
 
-export const APIPERSONAL = "http://localhost:3000/api/administrativo";
-export const APISTUDENT = "/api/estudiante";
+export const API_PERSONAL = "/api/administrativo";
+export const API_ESTUDIANTE = "/api/estudiante";
 
-export const DOCS = "api/estudiante/docs-solicitados";
-export const GETDOCS = "api/administrativo/get-docs-solicitados";
+export const API_DOCUMENTOS = "/api/estudiante/docs-solicitados";
+export const GET_DOCUMENTOS = "/api/administrativo/get-docs-solicitados";
