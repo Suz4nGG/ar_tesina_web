a085148c7552394d152531be43845edf83eac6c9
Suz4nGG <scxg240@gmail.com>
2023-02-09 11:11:23 -0600

    Componente de mensajes

--- components/Messages/ErrorMessages.jsx
+++ components/Messages/ErrorMessages.jsx
@@ -1,5 +1,13 @@
-const ErrorMessages = () => {
-  return <div></div>;
+const ErrorMessages = ({ errors, show, styles = "yellow" }) => {
+  return (
+    <>
+      {show ? (
+        <p className={`px-4 py-4 ${styles} my-4 rounded`}>{errors}</p>
+      ) : (
+        ""
+      )}
+    </>
+  );
 };
 
 export default ErrorMessages;
