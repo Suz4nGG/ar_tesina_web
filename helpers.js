aeccf80991ee800fae289c674b070109d42d9f83
Suz4nGG <scxg240@gmail.com>
2023-02-09 11:09:57 -0600

    Add: Helpers

--- helpers.js
+++ helpers.js
@@ -1,6 +1,19 @@
+import { normalizeText } from "/validations";
+import { dataProfesores } from "/data";
+
 export const timeOut = (time, setFunction) => {
   const timer = setTimeout(() => {
     setFunction;
   }, time);
   return () => clearTimeout(timer);
+};
+
+// ^ ee = Experiencia educativa
+export const profesoresInvolucrados = (ee) => {
+  const experienciaEducativa = normalizeText(ee).toLowerCase();
+  console.log(experienciaEducativa);
+  const responsablesSolicitud = dataProfesores.find(
+    (item) => item.ee === experienciaEducativa
+  );
+  return responsablesSolicitud.profesor;
 };
